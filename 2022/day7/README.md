6832a6ab899a9bf841d41a10ab8f11c7601b9054
Travis Veazey <travisvz@gmail.com>
2022-12-07 13:32:37 -0900

    Day 7

--- 2022/day7/README.md
+++ 2022/day7/README.md
@@ -1,4 +1,4 @@
-# Day 7: No Space Left On Device ---
+# Day 7: No Space Left On Device
 
 You can hear birds chirping and raindrops hitting leaves as the expedition proceeds. Occasionally, you can even hear much louder sounds in the distance; how big do the animals get out here, anyway?
 
@@ -41,13 +41,13 @@ The filesystem consists of a tree of files (plain data) and directories (which c
 
 Within the terminal output, lines that begin with $ are commands you executed, very much like some modern computers:
 
-    cd means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
-        cd x moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
-        cd .. moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
-        cd / switches the current directory to the outermost directory, /.
-    ls means list. It prints out all of the files and directories immediately contained by the current directory:
-        123 abc means that the current directory contains a file named abc with size 123.
-        dir xyz means that the current directory contains a directory named xyz.
+ - cd means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
+   - cd x moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
+   - cd .. moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
+   - cd / switches the current directory to the outermost directory, /.
+ - ls means list. It prints out all of the files and directories immediately contained by the current directory:
+   - 123 abc means that the current directory contains a file named abc with size 123.
+   - dir xyz means that the current directory contains a directory named xyz.
 
 Given the commands and output in the example above, you can determine that the filesystem looks visually like this:
 
@@ -74,11 +74,30 @@ Since the disk is full, your first step should probably be to find directories t
 
 The total sizes of the directories above can be found as follows:
 
-    The total size of directory e is 584 because it contains a single file i of size 584 and no other directories.
-    The directory a has total size 94853 because it contains files f (size 29116), g (size 2557), and h.lst (size 62596), plus file i indirectly (a contains e which contains i).
-    Directory d has total size 24933642.
-    As the outermost directory, / contains every file. Its total size is 48381165, the sum of the size of every file.
+ - The total size of directory e is 584 because it contains a single file i of size 584 and no other directories.
+ - The directory a has total size 94853 because it contains files f (size 29116), g (size 2557), and h.lst (size 62596), plus file i indirectly (a contains e which contains i).
+ - Directory d has total size 24933642.
+ - As the outermost directory, / contains every file. Its total size is 48381165, the sum of the size of every file.
 
 To begin, find all of the directories with a total size of at most 100000, then calculate the sum of their total sizes. In the example above, these directories are a and e; the sum of their total sizes is 95437 (94853 + 584). (As in this example, this process can count files more than once!)
 
 Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
+
+## Part Two
+
+Now, you're ready to choose a directory to delete.
+
+The total disk space available to the filesystem is 70000000. To run the update, you need unused space of at least 30000000. You need to find a directory you can delete that will free up enough space to run the update.
+
+In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; this means that the size of the unused space must currently be 21618835, which isn't quite the 30000000 required by the update. Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+
+To achieve this, you have the following options:
+
+ - Delete directory e, which would increase unused space by 584.
+ - Delete directory a, which would increase unused space by 94853.
+ - Delete directory d, which would increase unused space by 24933642.
+ - Delete directory /, which would increase unused space by 48381165.
+
+Directories e and a are both too small; deleting them would not free up enough space. However, directories d and / are both big enough! Between these, choose the smallest: d, increasing unused space by 24933642.
+
+Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. What is the total size of that directory?
--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,3 +1,200 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use utils::*;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Command<'a> {
+    Cd(&'a str),
+    CdRoot,
+    CdUp,
+    List,
+}
+
+#[derive(Debug)]
+enum CommandError {
+    UnknownCommand(String)
+}
+
+impl<'a> Command<'a> {
+    fn from_str(s: &'a str) -> Result<Self, CommandError> {
+        match s {
+            "ls" => Ok(Command::List),
+            "cd /" => Ok(Command::CdRoot),
+            "cd .." => Ok(Command::CdUp),
+            _ if &s[..3] == "cd " => Ok(Command::Cd(&s[3..])),
+            _ => Err(CommandError::UnknownCommand(s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Listing<'a> {
+    File(&'a str, usize),
+    Directory(&'a str),
+}
+
+#[derive(Debug)]
+enum ListingError {
+    UnrecognizedFormat(String)
+}
+
+impl<'a> Listing<'a> {
+    fn from_str(s: &'a str) -> Result<Self, ListingError> {
+        if let Some((data, name)) = s.split_once(' ') {
+            if let Ok(size) = data.parse::<usize>() {
+                Ok(Listing::File(name, size))
+            } else {
+                Ok(Listing::Directory(name))
+            }
+        } else {
+            Err(ListingError::UnrecognizedFormat(s.to_string()))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Terminal<'a> {
+    Command(Command<'a>),
+    Listing(Listing<'a>),
+}
+
+#[derive(Debug)]
+enum ParserError {
+    CommandError(CommandError),
+    ListingError(ListingError),
+    UnexpectedInput(String),
+}
+
+impl From<CommandError> for ParserError {
+    fn from(e: CommandError) -> Self {
+        Self::CommandError(e)
+    }
+}
+
+impl From<ListingError> for ParserError {
+    fn from(e: ListingError) -> Self {
+        Self::ListingError(e)
+    }
+}
+
+fn parse_line(line: &str) -> Result<Terminal, ParserError> {
+    match line.chars().next().unwrap() {
+        '$' => Ok(Terminal::Command(Command::from_str(&line[2..])?)),
+        'd' | '0'..='9' => Ok(Terminal::Listing(Listing::from_str(line)?)),
+        _ => Err(ParserError::UnexpectedInput(format!("Bad Input: {line}"))),
+    }
+}
+
+fn dir_size(dir: &String, filesystem: &HashMap<String, Vec<Listing>>) -> usize {
+    if let Some(list) = filesystem.get(dir) {
+        let mut size = 0;
+
+        for l in list.iter() {
+            match l {
+                Listing::File(_, file_size) => size += *file_size,
+                Listing::Directory(name) => {
+                    let path = vec![dir.as_str(), name].join("/");
+                    size += dir_size(&path, filesystem);
+                }
+            }
+        }
+
+        size
+    } else {
+        0
+    }
+}
+
+fn main() -> Result<(), ParserError> {
+    // let input = "$ cd /
+    // $ ls
+    // dir a
+    // 14848514 b.txt
+    // 8504156 c.dat
+    // dir d
+    // $ cd a
+    // $ ls
+    // dir e
+    // 29116 f
+    // 2557 g
+    // 62596 h.lst
+    // $ cd e
+    // $ ls
+    // 584 i
+    // $ cd ..
+    // $ cd ..
+    // $ cd d
+    // $ ls
+    // 4060174 j
+    // 8033020 d.log
+    // 5626152 d.ext
+    // 7214296 k";
+    let input = read_puzzle_input!().unwrap();
+
+    let mut pwd = vec![""];
+    let mut filesystem: HashMap<_, Vec<_>> = HashMap::new();
+    let mut dirs = vec![("".to_string(), 0)];
+
+    for line in input.lines() {
+        match parse_line(line.trim())? {
+            Terminal::Command(command) => {
+                match command {
+                    Command::Cd(dir) => { pwd.push(dir); },
+                    Command::CdUp => { pwd.pop(); },
+                    Command::CdRoot => { pwd.truncate(1); },
+                    Command::List => {},
+                }
+            }
+            Terminal::Listing(list) => {
+                if let Listing::Directory(name) = list {
+                    let path = vec![pwd.join("/").as_str(), name].join("/");
+                    dirs.push((path, 0));
+                }
+                filesystem.entry(pwd.join("/")).or_default().push(list);
+            }
+        }
+    }
+
+    for (dir, size) in dirs.iter_mut() {
+        *size = dir_size(&dir.to_string(), &filesystem);
+    }
+
+    // Part 1
+    let sum: usize = dirs.iter().filter_map(|(_, size)| {
+        if *size <= 100_000 {
+            Some(*size)
+        } else {
+            None
+        }
+    })
+    .sum();
+    println!("Sum of all directories up to 100KB: {sum}");
+
+    // Part 2
+    println!();
+
+    let used_space = dirs[0].1;
+
+    let total_space = 70_000_000;
+    let free_space = total_space - used_space;
+    let needed_space = 30_000_000;
+    let to_free = needed_space - free_space;
+
+    println!("Total disk space: {total_space}");
+    println!("Available disk space: {free_space}");
+    println!("Disk space needed for update: {needed_space}");
+    println!("Need to free {to_free} to allow update to proceed");
+
+    println!("\nAnalyzing disk space...\n");
+
+    if let Some((name, size)) = dirs.iter()
+        .filter(|(_, size)| *size >= to_free)
+        .min_by_key(|(_, size)| size)
+    {
+        println!("Deleting directory {name} would free {size} and allow the update to proceed");
+    } else {
+        println!("ERR: No suitable directory found that would free enough space");
+    }
+
+    Ok(())
 }
