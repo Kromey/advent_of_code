ad45b4e4df8ba42509bbde2a3d237539891276db
Travis Veazey <travisvz@gmail.com>
2022-12-06 16:42:19 -0900

    Golfing
    Golfed the function down to 5 LoC, but then rustfmt turned it back into 10
    
    Oh well, fewer allocations in this version I think

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -3,15 +3,15 @@ use std::collections::HashSet;
 use utils::read_puzzle_input;
 
 fn find_marker(data: &str, marker_len: usize) -> Result<usize, String> {
-    let received: Vec<_> = data.chars().collect();
-
-    let mut count = marker_len; // We're starting after the first 4 have been received
-    for seq in received.windows(marker_len) {
-        let set: HashSet<_> = seq.iter().collect();
-        if set.len() == seq.len() {
-            return Ok(count);
+    for (i, seq) in data
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(marker_len)
+        .enumerate()
+    {
+        if seq.iter().collect::<HashSet<_>>().len() == marker_len {
+            return Ok(i + marker_len);
         }
-        count += 1;
     }
 
     Err("Marker not found in input data".to_string())
@@ -22,10 +22,16 @@ fn main() -> Result<(), String> {
     let input = read_puzzle_input!().unwrap();
 
     // Part One: 4-character start-of-packet marker
-    println!("Start-of-packet marker received after {} characters", find_marker(&input, 4)?);
+    println!(
+        "Start-of-packet marker received after {} characters",
+        find_marker(&input, 4)?
+    );
 
     // Part Two: 14-character start-of-message marker
-    println!("Start-of-message marker received after {} characters", find_marker(&input, 14)?);
+    println!(
+        "Start-of-message marker received after {} characters",
+        find_marker(&input, 14)?
+    );
 
     Ok(())
 }
