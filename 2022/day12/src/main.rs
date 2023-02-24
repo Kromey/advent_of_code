a76c121ff042c23f75d44f2e8de530f1ee56ec45
Travis Veazey <travisvz@gmail.com>
2023-02-23 16:36:58 -0900

    rustfmt

--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -1,5 +1,5 @@
-use utils::*;
 use pathfinding::prelude::astar;
+use utils::*;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos(usize, usize);
@@ -12,9 +12,19 @@ impl Pos {
 
     /// Get the distance to the nearest "bottom" elevation
     fn distance_to_bottom(&self, elevations: &[Vec<u8>]) -> u32 {
-        elevations.iter().enumerate().map(|(y, row)| {
-            row.iter().enumerate().filter(|&(_, e)| *e == 0).map(move |(x, _)| Pos(x, y).distance(*self)).min().unwrap_or(u32::MAX)
-        }).min().unwrap()
+        elevations
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter(|&(_, e)| *e == 0)
+                    .map(move |(x, _)| Pos(x, y).distance(*self))
+                    .min()
+                    .unwrap_or(u32::MAX)
+            })
+            .min()
+            .unwrap()
     }
 
     /// Find all possible successors where the target is at most 1 step up
@@ -23,22 +33,35 @@ impl Pos {
 
         let elevation = elevations[self.1][self.0] as i16;
 
-        neighbors.into_iter().filter(|p| {
-            let e = elevations[p.1][p.0] as i16;
-            e - elevation <= 1
-        }).map(|p| (p, 1)).collect()
+        neighbors
+            .into_iter()
+            .filter(|p| {
+                let e = elevations[p.1][p.0] as i16;
+                e - elevation <= 1
+            })
+            .map(|p| (p, 1))
+            .collect()
     }
 
     /// Find all possible successors, but only if the target is 1 step down higher
-    fn rev_successors(&self, elevations: &[Vec<u8>], width: usize, height: usize) -> Vec<(Pos, u32)> {
+    fn rev_successors(
+        &self,
+        elevations: &[Vec<u8>],
+        width: usize,
+        height: usize,
+    ) -> Vec<(Pos, u32)> {
         let neighbors = self.neighbors(width, height);
 
         let elevation = elevations[self.1][self.0] as i16;
 
-        neighbors.into_iter().filter(|p| {
-            let e = elevations[p.1][p.0] as i16;
-            elevation - e <= 1
-        }).map(|p| (p, 1)).collect()
+        neighbors
+            .into_iter()
+            .filter(|p| {
+                let e = elevations[p.1][p.0] as i16;
+                elevation - e <= 1
+            })
+            .map(|p| (p, 1))
+            .collect()
     }
 
     /// Get up to 4 neighboring points, respecting the map's boundaries
@@ -68,19 +91,27 @@ fn main() {
     let mut start = Pos(0, 0);
     let mut goal = Pos(0, 0);
 
-    let elevations = input.lines().enumerate().map(|(y, line)| {
-        line.chars().enumerate().map(|(x, c)| {
-            if c == 'S' {
-                start = Pos(x, y);
-                'a'
-            } else if c == 'E' {
-                goal = Pos(x, y);
-                'z'
-            } else {
-                c
-            }
-        }).map(|c| c as u8 - b'a').collect::<Vec<u8>>()
-    }).collect::<Vec<_>>();
+    let elevations = input
+        .lines()
+        .enumerate()
+        .map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(x, c)| {
+                    if c == 'S' {
+                        start = Pos(x, y);
+                        'a'
+                    } else if c == 'E' {
+                        goal = Pos(x, y);
+                        'z'
+                    } else {
+                        c
+                    }
+                })
+                .map(|c| c as u8 - b'a')
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<_>>();
 
     let height = elevations.len();
     let width = elevations[0].len();
@@ -91,7 +122,8 @@ fn main() {
         |&p| p.successors(&elevations, width, height),
         |&p| p.distance(goal),
         |&p| p == goal,
-    ).unwrap();
+    )
+    .unwrap();
 
     // Subtract 1 from the length because the path includes our start
     println!("Reached goal in {} steps", path.len() - 1);
@@ -103,7 +135,8 @@ fn main() {
         |&p| p.rev_successors(&elevations, width, height),
         |&p| p.distance_to_bottom(&elevations),
         |&Pos(x, y)| elevations[y][x] == 0,
-    ).unwrap();
+    )
+    .unwrap();
 
     // Subtract 1 from the length because the path includes our start
     println!("Reached goal in {} steps", path.len() - 1);
